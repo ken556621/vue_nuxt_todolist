807112cf31299e2258c3be8770f47a792444db86
Ken <ken556621@gmail.com>
2022-11-24 14:05:02 +0800

    Add todolist

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,40 @@
 <template>
-  <Tutorial/>
+  <main>
+    <div class="inputArea">
+      <input 
+        type="text" 
+        v-model="newTask" 
+        placeholder="Please add new item!" 
+        @keypress.enter="addTask"
+      />
+      <button @click="addTask">
+        Add Item
+      </button>
+    </div>
+    <div class="todoList">
+      <Task
+        v-for="(task, i) in $store.state.tasks"
+        :key="i"
+        :task="task" 
+      />
+    </div>
+  </main>
 </template>
 
 <script>
-export default {
-  name: 'IndexPage'
-}
+  export default {
+    data() {
+      return {
+        newTask: ""
+      }
+    },
+    methods: {
+      addTask(){
+        if (this.newTask) {
+        this.$store.commit('ADD_TASK', this.newTask);
+        this.newTask = "";
+      }
+      }
+    }
+  }
 </script>
